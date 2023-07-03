848d76a32d9d494daaf6a3bb0fe287444aca55b6
Tiwaloluwa Ojo <136394099+tiwaojo-school@users.noreply.github.com>
2023-07-03 20:24:50 +0000

    feat: added some logging

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
-import { Application, Router  } from "https://deno.land/x/oak@v12.5.0/mod.ts";
+import { Application, Router, Context,Next } from "https://deno.land/x/oak@v12.5.0/mod.ts";
 import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 import data from "./data.json" assert { type: "json" };
 
-const router= new Router();
+const router = new Router();
 
 router.get("/", (context) => {
-    context.response.body = `<!DOCTYPE html>
+  context.response.body = `<!DOCTYPE html>
     <html>
       <head><title>Hello oak!</title><head>
       <body>
@@ -14,34 +14,60 @@ router.get("/", (context) => {
     </html>
   `;
 })
-.get("/api", (context) => {
-    context.response.body = data;    
-})
-.get("/api/:dinosaur", (context) => { 
+  .get("/api", (context) => {
+    context.response.body = data;
+  })
+  .get("/api/:dinosaur", (context) => {
     if (context.params && context.params.dinosaur) {
-        const filtered = data.filter((dino: { name: string; }) => dino.name.toLowerCase() === context.params.dinosaur?.toLowerCase());
-        if (filtered.length > 0) {
-            context.response.body = filtered;
-        }
+      const filtered = data.filter((dino: { name: string }) =>
+        dino.name.toLowerCase() === context.params.dinosaur?.toLowerCase()
+      );
+      if (filtered.length > 0) {
+        context.response.body = filtered;
+      }
     } else {
-        context.response.status = 404;
+      context.response.status = 404;
     }
-})
-.post("/api", async (context) => {
+  })
+  .post("/api", async (context) => {
     const body = await context.request.body();
     const dino = body.value;
     if (dino) {
-        data.push(dino);
-        context.response.body = data;
+      data.push(dino);
+      context.response.body = data;
     } else {
-        context.response.status = 400;
+      context.response.status = 400;
     }
-})
+  });
+async function responseTime(ctx:Context, next:Next) {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  console.log(`${ctx.request.method} ${ctx.request.url} - ${ms}`);
+//   ctx.response.set("X-Response-Time", `${ms}ms`);
+}
+
+function logger(format: string) {
+  format = format || ':method ":url"';
+
+  return async function (ctx:Context, next:Next) {
+    const str = format
+      .replace(":method", ctx.request.method)
+      .replace(":url", ctx.request.url.toString());
+
+    console.log(str);
+
+    await next();
+  };
+}
 
 const app = new Application();
 app.use(oakCors());
 app.use(router.routes());
 app.use(router.allowedMethods());
+// app.use(logger());
+app.use(logger(":method :url"));
+app.use(responseTime);
 
 console.log("Server running on port 8000");
 
